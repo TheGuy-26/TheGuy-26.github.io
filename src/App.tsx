9439d25d82fc8b6c25d3b665ed4e469374f815b6
Dujana <dudu00001@stud.uni-saarland.de>
2025-10-18 12:05:16 +0200

    update: link

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ export default function App(): React.JSX.Element {
                       <h1 className="text-2xl font-semibold mb-3"> {t.projects} </h1>
                       <ul className="list-none space-y-1">
                         <li>
-                              <a className="text-blue-600 hover:underline" href="https://github.com/TheGuy-26/concurrent_virus_simulation/pulse" target="_blank" rel="noopener noreferrer">
+                              <a className="text-blue-600 hover:underline" href="https://github.com/TheGuy-26/concurrent_virus_simulation/" target="_blank" rel="noopener noreferrer">
                                   Concurrent Virus Simulation
                               </a>
                           </li>

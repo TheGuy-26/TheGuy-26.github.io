9e64cdf61696fe2a081604604550440a0cc7132b
Dujana <dudu00001@stud.uni-saarland.de>
2025-11-04 00:42:50 +0100

    added cv checker

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -11,11 +11,11 @@ const MOBILE_BREAKPOINT = 1024;
 
 // skill colors
 const SKILL_COLORS = {
+    javascript: "text-yellow-600 dark:text-yellow-500 font-semibold",
+    typescript: "text-blue-700 dark:text-blue-400 font-semibold",
     rust: "text-red-600 dark:text-red-400 font-semibold",
     java: "text-orange-600 dark:text-orange-400 font-semibold",
     python: "text-blue-600 dark:text-blue-400 font-semibold",
-    javascript: "text-yellow-600 dark:text-yellow-500 font-semibold",
-    typescript: "text-blue-700 dark:text-blue-400 font-semibold",
     ocaml: "text-blue-700 dark:text-blue-300 font-semibold",
     django: "text-green-700 dark:text-green-400 font-semibold",
     scala: "text-red-700 dark:text-red-300 font-semibold",
